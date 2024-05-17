d243a28f69184cb61f07a497f583b13b3ba90064
Gal Ben Ami <benami171@gmail.com>
2024-05-17 12:55:08 +0300

    added tests to check algorithms.

--- Test.cpp
+++ Test.cpp
@@ -53,7 +53,7 @@ TEST_CASE("Test graph multiplication")
     CHECK(g4 == g5);
 }
 
-TEST_CASE("Test graph multiplication with scalar")
+TEST_CASE("Test graph multiplication with scalar both ways")
 {
     ariel::Graph g1;
     vector<vector<int>> graph = {
@@ -70,7 +70,67 @@ TEST_CASE("Test graph multiplication with scalar")
     ariel::Graph g3;
     g3.loadGraph(expectedGraph);
     CHECK(g2 == g3);
+}
 
+TEST_CASE("Test graph addition")
+{
+    ariel::Graph g1;
+    vector<vector<int>> graph = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g1.loadGraph(graph);
+    ariel::Graph g2;
+    vector<vector<int>> weightedGraph = {
+        {0, 1, 1},
+        {1, 0, 2},
+        {1, 2, 0}};
+    g2.loadGraph(weightedGraph);
+    ariel::Graph g3 = g1 + g2;
+    vector<vector<int>> expectedGraph = {
+        {0, 2, 1},
+        {2, 0, 3},
+        {1, 3, 0}};
+    ariel::Graph g4;
+    g4.loadGraph(expectedGraph);
+    CHECK((g3 == g4) == true);
+    CHECK((&g3 != &g1) == true);
+
+    SUBCASE("Test graph addition with assignment")
+    {
+        g1 += g2;
+        CHECK((g1 == g4) == true);
+        CHECK((g1 != g3) == false);
+    }
+
+    SUBCASE("Test unary +")
+    {
+        ariel::Graph g5 = +g1;
+        CHECK((g5 == g1) == true);
+    }
+
+    SUBCASE("Addition of two graphs with different dimensions")
+    {
+        ariel::Graph g6;
+        vector<vector<int>> graph2 = {
+            {0, 1, 0, 0, 1},
+            {1, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 1},
+            {1, 0, 0, 1, 0}};
+        g6.loadGraph(graph2);
+        CHECK_THROWS(g1 + g6);
+        CHECK_THROWS(g1 += g6);
+    }
+
+    SUBCASE("Test algorithms")
+    {
+        CHECK(g3.getIsDirected() == false);
+        CHECK(ariel::Algorithms::isConnected(g3) == true);
+        CHECK(ariel::Algorithms::isContainsCycle(g3) != "0");
+        CHECK(ariel::Algorithms::isBipartite(g3) == "Graph is not Bipartite");
+        CHECK(ariel::Algorithms::shortestPath(g3, 1, 2) == "1->2");
+    }
 }
 
 TEST_CASE("Invalid operations")
@@ -121,5 +181,4 @@ TEST_CASE("Test Increment and Decrement operators")
     g2--;
     --g2;
     CHECK(g1 == g2); // supposed to be equal, -- on zero matrix is still zero matrix
-
 }
